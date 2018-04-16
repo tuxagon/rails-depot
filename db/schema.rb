0efd3ef1c3c106eb1b7105e584eed0295f061c09
Kenneth Bogner <tuxagon91@gmail.com>
2018-04-15 23:23:11 -0400

    Migration to fix cart quantity tracking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180416000441) do
+ActiveRecord::Schema.define(version: 20180416031513) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
