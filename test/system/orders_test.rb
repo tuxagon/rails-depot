0d7f4f5ea63561b615c03e4a4d750a64f6fa1069
Kenneth Bogner <tuxagon91@gmail.com>
2018-04-20 00:37:52 -0400

    Capybara test for shipping order

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -4,7 +4,6 @@ class OrdersTest < ApplicationSystemTestCase
   include ActiveJob::TestHelper
 
   test "check pay type form" do
-
     LineItem.delete_all
     Order.delete_all
 
@@ -51,6 +50,9 @@ class OrdersTest < ApplicationSystemTestCase
   end
 
   test "credit card pay type form" do
+    LineItem.delete_all
+    Order.delete_all
+
     visit store_index_url
 
     first('.catalog li').click_on 'Add to Cart'
@@ -68,9 +70,35 @@ class OrdersTest < ApplicationSystemTestCase
 
     assert_selector "#order_credit_card_number"
     assert_selector "#order_expiration_date"
+
+    fill_in "CC #", with: "1234-2345-3456-4567"
+    fill_in "Expiry", with: "06/24"
+
+    perform_enqueued_jobs do
+      click_button "Place Order"
+    end
+
+    orders = Order.all
+    assert_equal 1, orders.size
+
+    order = orders.first
+
+    assert_equal "Dave Thomas",      order.name
+    assert_equal "123 Main Street",  order.address
+    assert_equal "dave@example.com", order.email
+    assert_equal "Credit card",      order.pay_type
+    assert_equal 1, order.line_items.size
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ["dave@example.com"],                 mail.to
+    assert_equal 'Sam Ruby <depot@example.com>',       mail[:from].value
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
   end
 
   test "purchase order pay type form" do
+    LineItem.delete_all
+    Order.delete_all
+
     visit store_index_url
 
     first('.catalog li').click_on 'Add to Cart'
@@ -86,5 +114,55 @@ class OrdersTest < ApplicationSystemTestCase
     select 'Purchase order', from: 'pay_type'
 
     assert_selector "#order_po_number"
+
+    fill_in "PO #", with: "123456"
+
+    perform_enqueued_jobs do
+      click_button "Place Order"
+    end
+
+    orders = Order.all
+    assert_equal 1, orders.size
+
+    order = orders.first
+
+    assert_equal "Dave Thomas",      order.name
+    assert_equal "123 Main Street",  order.address
+    assert_equal "dave@example.com", order.email
+    assert_equal "Purchase order",   order.pay_type
+    assert_equal 1, order.line_items.size
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ["dave@example.com"],                 mail.to
+    assert_equal 'Sam Ruby <depot@example.com>',       mail[:from].value
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+  end
+
+  test "ship order" do
+    Order.delete_all
+    order = Order.new(
+      name: "Dave Thomas",
+      address: "123 Main Street",
+      email: "dave@example.com",
+      pay_type: "Purchase order"
+    )
+    order.save!
+
+    ship_date = Date.today + 1
+
+    visit edit_order_url(order.id)
+
+    fill_in "order_ship_date", with: ship_date.strftime('%m/%d/%Y')
+
+    perform_enqueued_jobs do
+      click_button "Ship Order"
+    end
+
+    orders = Order.all
+    assert_equal 1, orders.size
+
+    order = orders.first
+
+    assert_equal ship_date, order.ship_date
   end
 end
