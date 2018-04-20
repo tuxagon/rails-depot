62a703c9d986ecf484954fc43f0bed5e3076d49e
Kenneth Bogner <tuxagon91@gmail.com>
2018-04-19 23:01:11 -0400

    Generated migration to add ship_date to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417230355) do
+ActiveRecord::Schema.define(version: 20180420025026) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20180417230355) do
     t.integer "pay_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "ship_date"
   end
 
   create_table "products", force: :cascade do |t|
