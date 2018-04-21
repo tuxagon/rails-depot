520e354facc00dd3943e803a93d3dab8699db99e
Kenneth Bogner <tuxagon91@gmail.com>
2018-04-21 18:06:31 -0400

    Added basic auth for non-html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,16 @@ class ApplicationController < ActionController::Base
   private
 
     def authorize
-      unless User.find_by(id: session[:user_id])
-        redirect_to login_url,
-          notice: "Please log in" unless no_admin_in_development
+      if request.format.html?
+        unless User.find_by(id: session[:user_id])
+          redirect_to login_url,
+            notice: "Please log in" unless no_admin_in_development
+        end
+      else
+        authenticate_or_request_with_http_basic do |username, password|
+          user = User.find_by(name: username)
+          user.try(:authenticate, password)
+        end unless request.format.html?
       end
     end
 
