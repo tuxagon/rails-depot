905034e2b5fc806e31941e48185b8aa37f5a2f91
Kenneth Bogner <tuxagon91@gmail.com>
2018-04-21 18:10:30 -0400

    Tested redirect to login for restricted route

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -6,4 +6,9 @@ class AdminControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should redirect when user not logged in" do
+    delete logout_url
+    get admin_url
+    assert_redirected_to login_url
+  end
 end
