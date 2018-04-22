616304c21110c1b5abfde95916e064938be245de
Kenneth Bogner <tuxagon91@gmail.com>
2018-04-22 14:34:45 -0400

    Added locale switcher

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
     resources :orders
     resources :line_items
     resources :carts
-    root 'store#index', as: 'store_index'
+    root 'store#index', as: 'store_index', via: :all
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
